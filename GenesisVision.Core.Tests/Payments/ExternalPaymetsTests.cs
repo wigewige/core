0ee693e4daafa5a1fc532731b654b7dc608113eb
r-kamenskiy <r@kamenskiy.net>
2018-02-23 13:24:10 +0300

    External payment test

---
+++ GenesisVision.Core.Tests/Payments/ExternalPaymetsTests.cs
@@ -0,0 +1,75 @@
+﻿using GenesisVision.DataModel;
+using GenesisVision.DataModel.Models;
+using GenesisVision.PaymentService.Models;
+using GenesisVision.PaymentService.Services;
+using GenesisVision.PaymentService.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenesisVision.Core.Tests.Payments
+{
+    [TestFixture]
+    public class ExternalPaymetsTests
+    {
+        private ApplicationDbContext context;
+        private IPaymentTransactionService paymentService;
+
+        private ApplicationUser user;
+
+        [SetUp]
+        public void Init()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseInMemoryDatabase("databaseManagerService")
+                .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+            context = new ApplicationDbContext(optionsBuilder.Options);
+
+            paymentService = new PaymentTransactionService(Substitute.For<ILogger<IPaymentTransactionService>>(), context);
+
+            var wallet = new Wallets { Amount = 0 };
+            user = new ApplicationUser
+            {
+                Id = Guid.NewGuid(),
+                IsEnabled = true,
+                Wallet = wallet
+            };
+            wallet.UserId = user.Id;
+            wallet.BlockchainAddresses = new List<BlockchainAddresses>
+            {
+                new BlockchainAddresses
+                 {
+                     Id = Guid.NewGuid(),
+                     Address = "0x00"
+                 }
+            };
+            context.Add(wallet);
+            context.Add(user);
+            context.SaveChanges();
+        }
+
+        [Test]
+        public void PaymentsCallbackTest1()
+        {
+            var wallet = context.Wallets.First(w => w.UserId == user.Id);
+            Assert.IsNull(wallet.WalletTransactions);
+
+            var payment = new ProcessPaymentTransaction
+            {
+                Amount = 100,
+                Address = "0x00",
+                Status = DataModel.Enums.PaymentTransactionStatus.ConfirmedAndValidated
+            };
+
+            paymentService.ProcessCallback(payment);
+
+            Assert.AreEqual(100, context.Wallets.First(w => w.UserId == user.Id).Amount);
+        }
+    }
+}
--- GenesisVision.DataModel/Models/Wallets.cs
+++ GenesisVision.DataModel/Models/Wallets.cs
@@ -5,9 +5,11 @@ namespace GenesisVision.DataModel.Models
 {
     public class Wallets
     {
+        public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public ApplicationUser User { get; set; }
 
+        public string Currency { get; set; }
         public decimal Amount { get; set; }
         
         public ICollection<WalletTransactions> WalletTransactions { get; set; }
