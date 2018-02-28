f8f5bbe8c3b408c699757e6ba1d306957380212a
r-kamenskiy <r@kamenskiy.net>
2018-02-28 15:18:53 +0300

    Git ignore fix

--- GenesisVision.Core.Tests/Services/TrustManagementServiceTests.cs
+++ GenesisVision.Core.Tests/Services/TrustManagementServiceTests.cs
@@ -14,6 +14,7 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using GenesisVision.Core.Services.Interfaces;
 
 namespace GenesisVision.Core.Tests.Services
 {
